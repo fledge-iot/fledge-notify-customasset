001e096aaa3192bdc3204c2737ff35e87b06a497
Sebastian J. Kropatschek <administrator@kropatschek.net>
2021-06-28 11:35:23 +0200

    add rest connection

--- customasset.cpp
+++ customasset.cpp
@@ -5,14 +5,37 @@
  *
  * Released under the Apache 2.0 Licence
  *
- * Author: Mark Riddoch           
+ * Author: Mark Riddoch
  */
 #include "customasset.h"
 #include <logger.h>
 #include <reading.h>
+#include <rapidjson/document.h>
+#include <rapidjson/error/en.h>
+#include <client_https.hpp>
+#include <client_http.hpp>
 
 using namespace std;
 using namespace rapidjson;
+
+using HttpsClient = SimpleWeb::Client<SimpleWeb::HTTPS>;
+using HttpClient = SimpleWeb::Client<SimpleWeb::HTTP>;
+
+
+const char * sample_store = QUOTE({
+	"sinusoid" : {
+		"sinusoid" : 0.5,
+		"timestamp" : "2019-01-01 10:00:00.123456+00:00"
+	},
+	"opcuajob" : {
+		"job" : "P60032085",
+		"timestamp" : "2019-01-01 10:00:00.123456+00:00"
+	},
+	"opcuaproductid" : {
+		"productid" : "S6866",
+		"timestamp" : "2019-01-01 10:00:00.123456+00:00"
+	}
+});
 
 
 /**
@@ -26,6 +49,9 @@ CustomAsset::CustomAsset(ConfigCategory *category)
 	m_description = category->getValue("description");
 	// TODO We have no access to the storage layer
 	m_ingest = NULL;
+
+	// FIXME: add error handling
+	m_client = new HttpClient("localhost:8081");
 }
 
 /**
@@ -33,6 +59,8 @@ CustomAsset::CustomAsset(ConfigCategory *category)
  */
 CustomAsset::~CustomAsset()
 {
+	delete m_client;
+	m_client = NULL;
 }
 
 /**
@@ -44,7 +72,7 @@ CustomAsset::~CustomAsset()
  */
 void CustomAsset::notify(const string& notificationName, const string& triggerReason, const string& message)
 {
-vector<Datapoint *>	datapoints;
+	vector<Datapoint *>	datapoints;
 
 	if (!m_ingest)
 	{
@@ -52,7 +80,7 @@ vector<Datapoint *>	datapoints;
 	}
 
 	DatapointValue dpv1(m_description);
-	datapoints.push_back(new Datapoint("description", dpv1)); 
+	datapoints.push_back(new Datapoint("description", dpv1));
 
 	Document doc;
 	doc.Parse(triggerReason.c_str());
@@ -62,12 +90,12 @@ vector<Datapoint *>	datapoints;
 		{
 			if (doc["reason"].IsString())
 			{
-		       		DatapointValue dpv3(doc["reason"].GetString());
+				DatapointValue dpv3(doc["reason"].GetString());
 				datapoints.push_back(new Datapoint("event", dpv3));
 			}
 			else if (doc["reason"].IsInt64())
 			{
-		       		DatapointValue dpv3((long) doc["reason"].GetInt64());
+				DatapointValue dpv3((long) doc["reason"].GetInt64());
 				datapoints.push_back(new Datapoint("event", dpv3));
 			}
 			else
@@ -78,6 +106,50 @@ vector<Datapoint *>	datapoints;
 	}
 	DatapointValue dpv4(notificationName);
 	datapoints.push_back(new Datapoint("rule", dpv4));
+
+	struct timeval now;
+	gettimeofday(&now, NULL);
+
+	#define DATE_TIME_BUFFER_LEN 52
+	char date_time[DATE_TIME_BUFFER_LEN];
+	char microsec[10];
+
+	struct tm utcnow;
+	gmtime_r(&now.tv_sec, &utcnow);
+
+	std::strftime(date_time, sizeof(date_time), "%Y-%m-%d %H:%M:%S", &utcnow);
+	snprintf(microsec, sizeof(microsec), ".%06lu", now.tv_usec);
+
+	ostringstream ss;
+	ss << date_time << microsec << "+00:00";
+
+
+	//string utcnow_str;
+	//utcnow_str = ss.str();
+
+	//m_store = "store";
+
+
+
+
+/*HttpClient client("localhost:8080");
+	try {
+		client.request("POST", resource, payload);
+	} catch (const exception& e) {
+		Logger::getLogger()->error("sendPayload: exception %s sending reading data to interested party %s", e.what(), url.c_str());
+	}
+*/
+
+
+	//m_store = getAssetReading();
+
+	Logger::getLogger()->warn("Done getAssetReading");
+
+	m_store = ss.str();
+
+	DatapointValue dpv5(m_store);
+	datapoints.push_back(new Datapoint("store", dpv5));
+
 	Reading customasset(m_customasset, datapoints);
 
 	Logger::getLogger()->info("CustomAsset notification: %s", customasset.toJSON().c_str());
@@ -95,4 +167,69 @@ void CustomAsset::reconfigure(const string& newConfig)
 	ConfigCategory category("new", newConfig);
 	m_customasset = category.getValue("customasset");
 	m_description = category.getValue("description");
+}
+
+
+
+const std::string CustomAsset::getAssetReading()
+{
+	try {
+			char url[256];
+
+			Logger::getLogger()->warn("Hello getAssetReading");
+
+			//request("GET", "/fledge/asset/sinusoid/sinusoid?limit=1");
+			//rl = "/fledge/asset/sinusoid/sinusoid?limit=1";
+			snprintf(url, sizeof(url), "/fledge/asset/%s?limit=1", "sinusoid");
+			//snprintf(url, sizeof(url), "/storage/reading?id=%ld&count=%ld", readingId, count);
+
+			Logger::getLogger()->warn("url: %s", url);
+
+			auto res = m_client->request("GET", url);
+			if (res->status_code.compare("200 OK") == 0)
+			{
+				ostringstream resultPayload;
+				resultPayload << res->content.rdbuf();
+				std::string result = resultPayload.str();
+				Logger::getLogger()->warn("result: %s", result.c_str());
+				return result;
+			}
+			ostringstream resultPayload;
+			resultPayload << res->content.rdbuf();
+			handleUnexpectedResponse("Fetch readings", res->status_code, resultPayload.str());
+		} catch (exception& ex) {
+			Logger::getLogger()->error("Failed to fetch asset: %s", ex.what());
+			throw;
+		} catch (exception* ex) {
+			Logger::getLogger()->error("Failed to fetch asset: %s", ex->what());
+			delete ex;
+			throw exception();
+	}
+	return "ERROR";
+}
+
+/**
+ * Standard logging method for all interactions
+ *
+ * @param operation	The operation beign undertaken
+ * @param responseCode	The HTTP response code
+ * @param payload	The payload in the response message
+ */
+void CustomAsset::handleUnexpectedResponse(const char *operation, const std::string& responseCode,  const std::string& payload)
+{
+	rapidjson::Document doc;
+
+	doc.Parse(payload.c_str());
+	if (!doc.HasParseError())
+	{
+		if (doc.HasMember("message"))
+		{
+			Logger::getLogger()->info("%s completed with result %s", operation, responseCode.c_str());
+			Logger::getLogger()->error("%s: %s", operation, doc["message"].GetString());
+		}
+	}
+	else
+	{
+		Logger::getLogger()->error("%s completed with result %s", operation, responseCode.c_str());
+	}
 }
--- include/customasset.h
+++ include/customasset.h
@@ -5,6 +5,18 @@
 #include <logger.h>
 #include <storage_client.h>
 
+#include <client_https.hpp>
+#include <client_http.hpp>
+using HttpClient = SimpleWeb::Client<SimpleWeb::HTTP>;
+
+#define TO_STRING(...) DEFER(TO_STRING_)(__VA_ARGS__)
+#define DEFER(x) x
+#define TO_STRING_(...) #__VA_ARGS__
+#define QUOTE(...) TO_STRING(__VA_ARGS__)
+
+
+
+
 typedef void (*FuncPtr)(void *, void *);
 /**
  * A simple customasset notification class that sends an customasset
@@ -14,6 +26,7 @@ class CustomAsset {
 	public:
 		CustomAsset(ConfigCategory *config);
 		~CustomAsset();
+
 		void	notify(const std::string& notificationName, const std::string& triggerReason, const std::string& message);
 		void	reconfigure(const std::string& newConfig);
 		void	registerIngest(FuncPtr ingest, void *data)
@@ -22,8 +35,14 @@ class CustomAsset {
 			m_data = data;
 		};
 	private:
+		const std::string getAssetReading();
+		void handleUnexpectedResponse(const char *operation, const std::string& responseCode,  const std::string& payload);
+
+		HttpClient *m_client;
+		//HttpClient m_client;
 		std::string	m_customasset;
 		std::string	m_description;
+		std::string	m_store;
 		FuncPtr		m_ingest;
 		void		*m_data;
 };
